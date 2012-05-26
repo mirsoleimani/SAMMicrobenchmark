4bf4b385a18ac4f7a0941741722338eeb9941dbb
s.ali mirsoleimani <sam@wave.com>
2012-05-26 16:12:51 +0430

    ---

--- Overheads/KernelLunch.cu
+++ Overheads/KernelLunch.cu
@@ -9,12 +9,12 @@
 #define MAXBLOCKSIZE 1024
 #define MAXGRIDSIZE 65535
 
-#define NUMREPEAT 1000
+#define NUMREPEAT 1000000
 
 __global__ void SimpleCopy(float *oData, float *iData)
 {
     int xId = blockIdx.x * blockDim.x + threadIdx.x;
-    oData[xId] = iData[xId];
+    oData[xId] = iData[xId]++;
 
 }
 
@@ -45,8 +45,8 @@ int main()
     cudaMemcpy(d_iData, h_iData, memSize, cudaMemcpyHostToDevice);
 
 
-    dim3 blockSize = 1;
-    dim3 gridSize = 1;
+    dim3 blockSize(1,1,1);
+    dim3 gridSize(1,1,1);
 
     cudaEvent_t start, stop;
     float time,latency;
@@ -55,20 +55,23 @@ int main()
     cudaEventCreate(&stop);
     CUDA_HANDLE_ERROR();
 
-    cudaEventRecord(start);
+    cudaEventRecord(start,0);
     
     for(int i=0;i<NUMREPEAT;i++)
     {
         SimpleCopy<<<gridSize,blockSize>>>(h_oData,h_iData);
+	cudaThreadSynchronize();
     }
 
-    cudaEventRecord(stop);
+    cudaEventRecord(stop,0);
     cudaEventSynchronize(stop);
+    time=0.0f;
     cudaEventElapsedTime(&time,start,stop);
 
     cudaMemcpy(h_oData, d_oData, memSize, cudaMemcpyDeviceToHost);
 
-    latency = time/(1.e3*(float)NUMREPEAT);
+    time /= 1.e3;
+    latency = time/((float)NUMREPEAT);
 
-    printf("kernel lunch overhead is:%0.10f\n",latency);
+    printf("kernel lunch overhead is:%0.50f\n",time);
 }
