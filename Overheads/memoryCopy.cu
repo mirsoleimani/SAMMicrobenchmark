52ba8811d63d8dc4c222df0adbfdefbcb5677d78
s.ali mirsoleimani <ali.mirsoleimani@gmail.com>
2012-05-28 10:47:10 +0430

    Create a benchmark to test memory copy API launch overhead

--- Overheads/memoryCopy.cu
+++ Overheads/memoryCopy.cu
@@ -4,19 +4,13 @@
 #include <utility.h>
 
 
-#define DEFAULTMEMSIZE (1<<20)
+#define DEFAULTMEMSIZE (1<<2)//4B
 
 #define MINBLOCKSIZE 1
 #define MAXBLOCKSIZE 1024
 #define MAXGRIDSIZE 65535
 
 #define NUMREPEAT 1000000
-
-__global__ void SimpleCopy(float *oData, float *iData)
-{
-	int xId = blockIdx.x * blockDim.x + threadIdx.x;
-	oData[xId] = iData[xId];
-}
 
 int main()
 {
@@ -42,11 +36,6 @@ int main()
     cudaMalloc((void **)&d_oData,memSize);
     CUDA_HANDLE_ERROR();
 
-    cudaMemcpy(d_iData, h_iData, memSize, cudaMemcpyHostToDevice);
-
-
-    dim3 blockSize(1,1,1);
-    dim3 gridSize(1,1,1);
 
     cudaEvent_t start, stop;
     float time,latency;
@@ -59,7 +48,7 @@ int main()
     
     for(int i=0;i<NUMREPEAT;i++)
     {
-        SimpleCopy<<<gridSize,blockSize>>>(d_oData,d_iData);
+        cudaMemcpy(d_iData, h_iData, memSize, cudaMemcpyHostToDevice);
 	    //cudaThreadSynchronize();
     }
 
@@ -68,8 +57,8 @@ int main()
     time=0.0f;
     cudaEventElapsedTime(&time,start,stop);
     
-    cudaMemcpy(h_oData, d_oData, memSize, cudaMemcpyDeviceToHost);
-    CUDA_HANDLE_ERROR();
+    //cudaMemcpy(h_oData, d_oData, memSize, cudaMemcpyDeviceToHost);
+    //CUDA_HANDLE_ERROR();
 
 
     time /= 1.e3;
