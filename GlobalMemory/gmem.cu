8bcf4a701635a817fab4ecaa63f4f21f1ec4affb
s.ali mirsoleimani <ali.mirsoleimani@gmail.com>
2012-06-11 17:16:05 +0430

    Add StrideAccess kernel

--- GlobalMemory/gmem.cu
+++ GlobalMemory/gmem.cu
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <cuda.h>
+#include <repeat.h>
+#include <utility.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
 #define DEFAULTMEMSIZE (1<<20)
+#define DEFAULTSTEPSIZE (1<<20)
 
 #define MINBLOCKSIZE 1
 #define MAXBLOCKSIZE 1024
 #define MAXGRIDSIZE 65535
+
+float *h_iData;
+float *h_oData;
+float *d_iData;
+float *d_oData;
 
 //***SimpleCopy_Start***
 __global__ void SimpleCopy(float *oData, float *iData)
@@ -27,53 +35,106 @@ __global__ void StrideCopy(float *oData, float *iData,int stride)
 //***StrideCopy_End***
 
 //***StrideAccess_Start***
-__global__ void StrideAccess(float *oData, float *iData)
+__global__ void StrideAccess(float *oData, float *iData,int stride)
 {
-
+    int xId = (blockIdx.x*blockDim.x+threadIdx.x)*stride;
+    oData[xId] = iData[xId];
 }
 //***StrideAccess_End***
 
+void RunStrideAccess(int stride,int nWords, int memSize,int nRepeats)
+{
+    for(unsigned int i=0;i<nWords;i++)
+    {
+        h_oData[i]= (float)((i+stride)%nWords);
+    }
+
+    cudaMemcpy(d_iData, h_oData, memSize, cudaMemcpyHostToDevice);
+
+    int blockSize = 256;
+    int gridSize = nWords/blockSize;
+
+    cudaEvent_t start, stop;
+    float time,latency;
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    CUDA_HANDLE_ERROR();
+
+    cudaEventRecord(start,0);
+
+    for(int i=0;i<nRepeats;i++)
+    {
+        StrideAccess(d_oData,d_iData,stride);
+    }
+
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+
+    time=0.0f;
+    cudaEventElapsedTime(&time,start,stop);
+
+    time /= 1.e3;
+    latency = time/((float)nRepeats);
+
+    printf("stride:%0.5f, latency:%0.5f\n",stride,latency);
+
+
+}
 void TestLatency()
 {
+    size_t memSize = 32*DEFAULTMEMSIZE;
+    size_t nWords = (memSize)/sizeof(float);
+    int nRepeats=10;
+
+    //Initialize Host memory
+    h_iData = new float[nWords];
+    h_oData = new float[nWords];
+
+    //initialize Device memory
+    cudaMalloc((void **)&d_iData,memSize);
+    CUDA_HANDLE_ERROR();
+    cudaMalloc((void **)&d_oData,memSize);
+    CUDA_HANDLE_ERROR();
+
+    for(int stride=2;stride <= memSize/2; stride*=2)
+    {
+        RunStrideAccess(stride, nWords, memSize, nRepeats);
+    }
+
+    cudaFree(d_iData);
+    cudaFree(d_oData);
+
+    free(h_iData);
+    free(h_oData);
 
 }
 
 void TestBandwidth()
 {
+    size_t memSize = 128*DEFAULTMEMSIZE;
+    size_t nWords = (memSize)/sizeof(float);
 
+    //Initialize Host memory
+    h_iData = new float[nWords];
+    h_oData = new float[nWords];
+
+    //initialize Device memory
+    cudaMalloc((void **)&d_iData,memSize);
+    CUDA_HANDLE_ERROR();
+    cudaMalloc((void **)&d_oData,memSize);
+    CUDA_HANDLE_ERROR();
+
+    cudaFree(d_iData);
+    cudaFree(d_oData);
+
+    free(h_iData);
+    free(h_oData);
 }
 
 int main()
 {
-    size_t memSize = 32*DEFAULTMEMSIZE;
-    size_t nWords = (memSize)/sizeof(float);
-    
-    //Initialize Host memory
-    float *h_iData;
-    float *h_oData;
-    h_iData = new float[nWords];
-    h_oData = new float[nWords];
 
-    for(unsigned int i=0;i<nWords;i++)
-    {
-        h_iData[i]= (float) (i&(0xFF));
-    }
-
-    //initialize Device memory
-    float *d_iData;
-    float *d_oData;
-    cudaMalloc((void **)&d_iData,memSize);
-    //CUDA_HANDLE_ERROR();
-    cudaMalloc((void **)&d_oData,memSize);
-    //CUDA_HANDLE_ERROR();
-
-    cudaMemcpy(d_iData, h_iData, memSize, cudaMemcpyHostToDevice);
-
-
-    int blockSize = 256;
-    int gridSize = nWords/blockSize;
-
-    //cudaEvent_t start, stop;
 
     return 0;
 }
