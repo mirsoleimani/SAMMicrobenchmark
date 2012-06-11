93904ebe5bfe43141565f65095092ee9333e6f16
s.ali mirsoleimani <ali.mirsoleimani@gmail.com>
2012-06-11 13:43:09 +0430

    add new function for latency measurement

--- GlobalMemory/gmem.cu
+++ GlobalMemory/gmem.cu
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 #define DEFAULTMEMSIZE (1<<20)
 
@@ -7,14 +9,39 @@
 #define MAXBLOCKSIZE 1024
 #define MAXGRIDSIZE 65535
 
-
+//***SimpleCopy_Start***
 __global__ void SimpleCopy(float *oData, float *iData)
 {
     int xId = blockIdx.x * blockDim.x + threadIdx.x;
     oData[xId] = iData[xId];
 
 }
+//***SimpleCopy_End***
 
+//***StrideCopy_Start***
+__global__ void StrideCopy(float *oData, float *iData,int stride)
+{
+    int xId = (blockIdx.x*blockDim.x+threadIdx.x)*stride;
+    oData[xId] = iData[xId];
+}
+//***StrideCopy_End***
+
+//***StrideAccess_Start***
+__global__ void StrideAccess(float *oData, float *iData)
+{
+
+}
+//***StrideAccess_End***
+
+void TestLatency()
+{
+
+}
+
+void TestBandwidth()
+{
+
+}
 
 int main()
 {
